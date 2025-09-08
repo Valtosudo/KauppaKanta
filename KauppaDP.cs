ddc8a281d5ad4774d325516acb4486b4409fca0c
Valtosudo <valtsu.vaajalahti@gmail.com>
2025-09-08 10:59:24 +0300

    Pari metodia Lisaatuote ja haetuote

--- KauppaDP.cs
+++ KauppaDP.cs
@@ -1,6 +1,5 @@
 namespace KauppaKanta;
 
-using System.Runtime.InteropServices;
 using Microsoft.Data.Sqlite;
 
 public class KauppaDP
@@ -19,5 +18,46 @@ public class KauppaDP
         commandForTableCreation.CommandText = "CREATE TABLE IF NOT EXISTS tuotteet (id INTEGER PRIMARY KEY, nimi TEXT, hinta REAL)";
         commandForTableCreation.ExecuteNonQuery();
         connection.Close();
+    }
+
+    public void LisaaTuote(string nimi, double hinta)
+    {
+        //Luodaan Yhteys tietokantaan
+        var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+        //Lisätään tuote tietokantaan
+        var commandForInsert = connection.CreateCommand();
+        commandForInsert.CommandText = "INSERT INTO tuotteet (nimi, hinta) VALUES (@nimi, @hinta)";
+        commandForInsert.Parameters.AddWithValue(@"nimi", nimi);
+        commandForInsert.Parameters.AddWithValue(@"hinta", hinta);
+        commandForInsert.ExecuteNonQuery();
+        connection.Close();
+    }
+
+    public string HaeTuotteet(string nimi)
+    {
+        //Luodaan yhteys tietokantaan
+        var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+        var commandForSelect = connection.CreateCommand();
+        commandForSelect.CommandText = "SELECT * FROM tuotteet WHERE nimi LIKE @Nimi";
+        commandForSelect.Parameters.AddWithValue("Nimi", nimi);
+        var reader = commandForSelect.ExecuteReader();
+        string tuotteet = "";
+        while (reader.Read())
+        {
+            tuotteet += $"id: {reader.GetInt32(0)}, nimi: {reader.GetString(1)}, Hinta: {reader.GetDouble(2)}\n";
+        }
+        reader.Close();
+        connection.Close();
+
+        if (tuotteet == "")
+        {
+            return "Tuotetta ei löytynyt.";
+        }
+        else
+        {
+            return tuotteet;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,8 +6,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        KauppaDP kauppa = new KauppaDP();
-        
-        Console.WriteLine("Hello, World!");
+        KauppaDP kauppaDB = new KauppaDP();
+
+        while (true)
+        {
+            Console.WriteLine("Haluatko lisätä tuotteen? (L), hakea tuotteen (H) vai lopettaa (X)");
+            string? vastaus = Console.ReadLine();
+            switch (vastaus)
+            {
+                case "L":
+                        Console.WriteLine("Anna tuotteen nimi:");
+                        string? nimi = Console.ReadLine();
+                        Console.WriteLine("Anna tuotteen hinta:");
+                        double hinta = Convert.ToDouble(Console.ReadLine());
+                        //Lisätään tuote tietokantaan
+                        kauppaDB.LisaaTuote(nimi, hinta);
+                        break;
+                    
+                case "H":
+                //Haetaan tuotteet tietokannasta
+                    Console.WriteLine("Anna haettavan tuotteen nimi:");
+                    string? haettavanimi = Console.ReadLine();
+                    string tuotteet = kauppaDB.HaeTuotteet(haettavanimi);
+                    Console.WriteLine(tuotteet);
+                    break;
+
+                case "X":
+                {
+                    return;
+                }
+
+                default:
+                    Console.WriteLine("Väärä syöte, anna L, H tai X.");
+                    break;
+            }
+        }
     }
 }
