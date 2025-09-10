f2cfaa144f4f586476791eaf8d6ff63bfe863bb1
Valtosudo <valtsu.vaajalahti@gmail.com>
2025-09-10 09:13:22 +0300

    using lisätty

--- KauppaDP.cs
+++ KauppaDP.cs
@@ -4,12 +4,12 @@ using Microsoft.Data.Sqlite;
 
 public class KauppaDP
 {
-    private String _connectionString = "Data Source=kauppa.db";
+    private String _connectionString = "Data Source = kauppa.db";
 
     public KauppaDP()
     {
         //Luodaan Yhteys tietokantaan
-        using (var connection = new SqliteConnection(_connectionString));
+        using (var connection = new SqliteConnection(_connectionString))
         {
             connection.Open();
             //Luodaan Taulut, jos niitä ei vielä ole
@@ -25,41 +25,46 @@ public class KauppaDP
     public void LisaaTuote(string nimi, double hinta)
     {
         //Luodaan Yhteys tietokantaan
-        var connection = new SqliteConnection(_connectionString);
-        connection.Open();
-        //Lisätään tuote tietokantaan
-        var commandForInsert = connection.CreateCommand();
-        commandForInsert.CommandText = "INSERT INTO tuotteet (nimi, hinta) VALUES (@nimi, @hinta)";
-        commandForInsert.Parameters.AddWithValue(@"nimi", nimi);
-        commandForInsert.Parameters.AddWithValue(@"hinta", hinta);
-        commandForInsert.ExecuteNonQuery();
-        connection.Close();
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            //Lisätään tuote tietokantaan
+            var commandForInsert = connection.CreateCommand();
+            commandForInsert.CommandText = "INSERT INTO tuotteet (nimi, hinta) VALUES (@nimi, @hinta)";
+            commandForInsert.Parameters.AddWithValue(@"nimi", nimi);
+            commandForInsert.Parameters.AddWithValue(@"hinta", hinta);
+            commandForInsert.ExecuteNonQuery();
+            connection.Close();
+        }
     }
 
     public string HaeTuotteet(string nimi)
     {
         //Luodaan yhteys tietokantaan
-        var connection = new SqliteConnection(_connectionString);
-        connection.Open();
-        var commandForSelect = connection.CreateCommand();
-        commandForSelect.CommandText = "SELECT * FROM tuotteet WHERE nimi LIKE @Nimi";
-        commandForSelect.Parameters.AddWithValue("Nimi", nimi);
-        var reader = commandForSelect.ExecuteReader();
-        string tuotteet = "";
-        while (reader.Read())
+        using (var connection = new SqliteConnection(_connectionString))
         {
-            tuotteet += $"id: {reader.GetInt32(0)}, nimi: {reader.GetString(1)}, Hinta: {reader.GetDouble(2)}\n";
-        }
-        reader.Close();
-        connection.Close();
-
-        if (tuotteet == "")
-        {
-            return "Tuotetta ei löytynyt.";
-        }
-        else
-        {
-            return tuotteet;
+            connection.Open();
+            var commandForSelect = connection.CreateCommand();
+            commandForSelect.CommandText = "SELECT * FROM tuotteet WHERE nimi LIKE @Nimi";
+            commandForSelect.Parameters.AddWithValue("Nimi", nimi);
+            using (var reader = commandForSelect.ExecuteReader())
+            {
+                string tuotteet = "";
+                while (reader.Read())
+                {
+                    tuotteet += $"id: {reader.GetInt32(0)}, nimi: {reader.GetString(1)}, Hinta: {reader.GetDouble(2)}\n";
+                }
+                //reader.Close();
+                //connection.Close();
+                if (tuotteet == "")
+                {
+                    return "Tuotetta ei löytynyt.";
+                }
+                else
+                {
+                    return tuotteet;
+                }
+            }
         }
     }
 }
